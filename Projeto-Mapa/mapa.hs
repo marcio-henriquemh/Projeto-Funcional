60b0415a616be0db77f0e8eec0fca9e8f0aa9687
Márcio Henrique Matos de Freitas <70753463+marcio-henriquemh@users.noreply.github.com>
2023-09-08 21:44:26 -0300

    Add files via upload

--- Projeto-Mapa/mapa.hs
+++ Projeto-Mapa/mapa.hs
@@ -15,6 +15,21 @@ type Mapa = [Cidade]
 mapaVazio :: Mapa
 mapaVazio = []
 
+--trd que recebe uma tupla com três elementos de tipos genéricos x, y e z, e retorna o terceiro elemento z
+trd :: (x, y, z) -> z
+trd (_, _, z) = z
+
+--função chamada addCidade que recebe dois argumentos: um Mapa (que é uma lista de cidades) e uma Cidade
+addCidade :: Mapa -> Cidade -> Mapa
+addCidade mapa cidade = cidade : mapa
+
+--remove a cidade com o nome especificado do mapa. Se a cidade não for encontrada, o mapa permanece inalterado.
+removerCidade :: Mapa -> Nome -> Mapa
+removerCidade [] _ = []
+removerCidade (cidade@(nome, _, _):resto) nomeParaRemover
+    | nome == nomeParaRemover = resto
+    | otherwise = cidade : removerCidade resto nomeParaRemover
+
 main :: IO ()
 main = do
     let meuMapaVazio = mapaVazio
@@ -22,11 +37,19 @@ main = do
     let saoCristovao = ("São Cristovão", (15.0, 25.0), ["Aracaju", "São Cristovão"])
     let maceio = ("Maceió", (12.0, 18.0), ["Aracaju", "São Cristovão"])
     let pauloAfonso = ("Paulo Afonso", (9.0, 38.0), [])  -- Adicionando Paulo Afonso
-    let meuMapa = [aracaju, saoCristovao, maceio, pauloAfonso]  -- Adicionando a cidade ao mapa
 
-    putStrLn "Mapa:"
-    mapM_ (exibirCidade) meuMapa
+    -- Adicionando a cidade de Capela ao mapa
+    let capela = ("Capela", (10.6, 36.2), ["Aracaju"])  -- Adicionei Capela com uma rota para Aracaju
+    let meuMapaComCapela = addCidade [aracaju, saoCristovao, maceio, pauloAfonso] capela
 
+    putStrLn "Mapa com a Cidade de Capela Adicionada:"
+    mapM_ (exibirCidade) meuMapaComCapela
+
+    -- Removendo a cidade de Capela do mapa (opcional)
+    let meuMapaSemCapela = removerCidade meuMapaComCapela "Capela"
+
+    putStrLn "Mapa sem a Cidade de Capela:"
+    mapM_ (exibirCidade) meuMapaSemCapela
 
 --função para exibir infomações iniciais (antes de gerar a imagem do mapa com as cidades.)
 exibirCidade :: Cidade -> IO ()
